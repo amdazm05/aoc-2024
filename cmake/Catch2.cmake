714a9e71a6d2c7fc014fe5c4a1a6caf8997b6f71
amdazm05 <amdali1998@yahoo.com>
2024-11-28 21:24:49 +0000

    cmake: catch2 dependancy added

---
+++ cmake/Catch2.cmake
@@ -0,0 +1,21 @@
+macro(FetchCatch2 CATCH2_VERSION)
+    include(FetchContent)
+    Set(FETCHCONTENT_QUIET FALSE)
+    set(CATCH_CONFIG_ENABLE_BENCHMARKING FALSE)
+    FetchContent_Declare(
+        Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG        v${CATCH2_VERSION} # or a later release
+        GIT_PROGRESS TRUE
+    )
+    FetchContent_MakeAvailable(Catch2)
+    message("${catch2_SOURCE_DIR}")
+    list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/extras)
+endmacro(FetchCatch2)
+
+function(CompileTestFile TEST_NAME FILE_PATH)
+    include(CTest)
+    include(Catch)
+    add_executable(${TEST_NAME} ${FILE_PATH})
+    target_link_libraries(${TEST_NAME} PRIVATE Catch2::Catch2WithMain)
+endfunction(CompileTestFile FILE_PATH)
